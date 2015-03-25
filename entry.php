228adeefdbace122f5b2edc8f7b553c6ba69624d
Mark Lippert <mark@foresitegrp.com>
2015-03-25 10:38:09 -0500

    Update registration link

--- entry.php
+++ entry.php
@@ -12,6 +12,6 @@ include "header.php";
 Race entry is online only. The deadline is Noon on Thursday, September 24th 2015.<br>
 <br>
 
-Please visit the <a href="http://www.regattanetwork.com/event/8833">Online Registration page</a> to enter and fill in all the required fields. Have your credit card handy. Note: This is a secure registration site widely used by US Sailing.
+Please visit the <a href="http://www.regattanetwork.com/event/10100">Online Registration page</a> to enter and fill in all the required fields. Have your credit card handy. Note: This is a secure registration site widely used by US Sailing.
 
 <?php include "footer.php"; ?>
--- regatta.php
+++ regatta.php
@@ -30,7 +30,7 @@ Held off the Milwaukee shoreline in the waters of Lake Michigan, Louie's attract
 Race entry is online only. The deadline is Noon on Thursday, September 24th 2015.<br>
 <br>
 
-Please visit the <a href="http://www.regattanetwork.com/event/8833">Online Registration page</a> to enter and fill in all the required fields. Have your credit card handy. Note: This is a secure registration site widely used by US Sailing.<!-- <br>
+Please visit the <a href="http://www.regattanetwork.com/event/10100">Online Registration page</a> to enter and fill in all the required fields. Have your credit card handy. Note: This is a secure registration site widely used by US Sailing.<!-- <br>
 <br>
 
 <a href="pdfs/2014_Notice_of_Race.pdf">Notice of Race (NOR)</a><br>
