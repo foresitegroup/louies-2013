cdf08ecb093468e570c3b395020fe5a8dfd94a97
foresitegroup <mark@foresitegrp.com>
2016-08-23 17:06:37 -0500

    Update for 2016

--- entry.php
+++ entry.php
@@ -6,12 +6,13 @@ include "header.php";
 
 <h3>Race Entry</h3>
 
-<!-- <a href="pdfs/2015_Notice_of_Race.pdf">2015 Louie's Last Regatta Notice of Race (NOR)</a><br>
-<br> -->
-
-Race entry is online only. The deadline is Noon on Thursday, September 22 2016.<!-- <br>
+<a href="pdfs/2016_Notice_of_Race.pdf">2016 Louie's Last Regatta Notice of Race (NOR)</a><br>
+<a href="pdfs/2016_Sailing_Instructions.pdf">2016 Sailing Instructions</a><br>
 <br>
 
-Please visit the <a href="http://www.regattanetwork.com/event/10100">Online Registration page</a> to enter and fill in all the required fields. Have your credit card handy. Note: This is a secure registration site widely used by US Sailing. -->
+Race entry is online only. The deadline is Noon on Thursday, September 22, 2016.<br>
+<br>
+
+Please visit the <a href="https://www.regattanetwork.com/clubmgmt/applet_registration_form.php?regatta_id=13313">Online Registration page</a> to enter and fill in all the required fields. Have your credit card handy. Note: This is a secure registration site widely used by US Sailing.
 
 <?php include "footer.php"; ?>
